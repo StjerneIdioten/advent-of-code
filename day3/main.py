c91a1738c9f4421b40b4c5b5d59ac38c8908e469
stjerneidioten <stjerneidioten@gmail.com>
2022-12-15 22:34:15 +0100

    Solve both challenges of day 3

--- day3/main.py
+++ day3/main.py
@@ -1,10 +1,27 @@
 import sys
 import os
 
+def get_priority(c):
+    return ord(c) - 0x60 if (c > 'Z') else ord(c) - 0x26
+
 if __name__ == "__main__":
-    priority_sum = 0
+    priority_sum, group_sum = 0,0
     with open(os.path.join(sys.path[0], "data")) as file:
+        idx, c3 = 0,set()
         while (line := file.readline()[:-1]):
-            c = set(line[:len(line)//2]).intersection(set(line[len(line)//2:None])).pop()
-            priority_sum += ord(c) - 0x60 if (c > 'Z') else ord(c) - 0x26
-    print(f"{priority_sum}")
+            c1 = set(line[:len(line)//2])
+            c2 = set(line[len(line)//2:None])
+            priority_sum += get_priority(c1.intersection(c2).pop())
+
+            if idx == 0:
+                c3 = c1.union(c2)
+            else:
+                c3.intersection_update(c1.union(c2))
+
+            if idx == 2:
+                group_sum += get_priority(c3.pop())
+                idx = 0
+            else:
+                idx += 1
+
+    print(f"Priority Sum: {priority_sum}, Group Sum: {group_sum}")
