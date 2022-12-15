0823f8e8a3a734b45fe3c78378b8102cc1180112
stjerneidioten <stjerneidioten@gmail.com>
2022-12-15 21:03:19 +0100

    Solve day 3 part 1

--- day3/main.py
+++ day3/main.py
@@ -2,7 +2,9 @@ import sys
 import os
 
 if __name__ == "__main__":
+    priority_sum = 0
     with open(os.path.join(sys.path[0], "data")) as file:
-        while (line := file.readline()):
-            c1 = line[:len(line)]
-            c2 = line[len[line]:]
+        while (line := file.readline()[:-1]):
+            c = set(line[:len(line)//2]).intersection(set(line[len(line)//2:None])).pop()
+            priority_sum += ord(c) - 0x60 if (c > 'Z') else ord(c) - 0x26
+    print(f"{priority_sum}")
