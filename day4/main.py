85839e97699935bf4d464595c6f1fcd7c656ee28
stjerneidioten <stjerneidioten@gmail.com>
2022-12-18 22:21:44 +0100

    Fix formatting in day 4

--- day4/main.py
+++ day4/main.py
@@ -8,15 +8,16 @@ if __name__ == "__main__":
     with open(os.path.join(sys.path[0], "data")) as file:
         while (line := file.readline().rstrip()):
             ranges = list(map(int, re.split("[\n,-]+", line)))
-            e1, e2 = (ranges[0:2],ranges[2:4])
+            e1, e2 = (ranges[0:2], ranges[2:4])
+            
+            if max(e1[0], e2[0]) <= min(e1[1], e2[1]):
+                overlapping_pairs += 1
+
             if (min(e1[0], e2[0]) == e2[0]):
                 e1, e2 = e2, e1
     
             if (e1[0] == e2[0]) or (e1[1] >= e2[1]) or (e1[0] > e2[0]):
                 contained_pairs += 1
-
-            if max(e1[0], e2[0]) <= min(e1[1], e2[1]) :
-                overlapping_pairs += 1
 
     print(f"C: {contained_pairs}, O: {overlapping_pairs}")
 
