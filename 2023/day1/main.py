b2edec38a7d0c26102dd01eee8a0561d363cf28c
StjerneIdioten <StjerneIdioten@gmail.com>
2023-12-02 00:46:49 +0100

    Attempt at part 2 for day 1

--- 2023/day1/main.py
+++ 2023/day1/main.py
@@ -5,21 +5,56 @@ import re
 if __name__ == "__main__":
     with open(Path(__file__).parent / "data") as file:
         digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        spellings = ["one", "two", "six", "nine", "four", "five", "three", "seven", "eight"]
+        spellings = {"one": "1", "two": "2", "six": "6", "nine": "9", "four": "4", "five": "5", "three": "3", "seven": "7", "eight": "8"}
 
-        calibration_value_digits = 0
+        calibration_value_only_digits = 0
+        calibration_value_both = 0
         while (line := file.readline().rstrip('\n\r')):
+            first_digit = ""
+            last_digit = ""
             last_idx = len(line) - 1
-            earlist_digit_idx = last_idx
+            earliest_digit_idx = last_idx
             latest_digit_idx = 0
-            #print(f"'{line}'")
             for digit in digits:
-                if (earlist_digit_idx != 0) and ((idx := line.find(digit, 0, earlist_digit_idx)) != -1) and (idx < earlist_digit_idx):
-                    if (earlist_digit_idx := idx) == 0 and latest_digit_idx == last_idx:
+                if (earliest_digit_idx != 0) and ((idx := line.find(digit, 0, earliest_digit_idx)) != -1) and (idx < earliest_digit_idx):
+                    if (earliest_digit_idx := idx) == 0 and latest_digit_idx == last_idx:
                         break
 
                 if (latest_digit_idx != last_idx) and ((idx := line.rfind(digit, latest_digit_idx, last_idx + 1)) != -1) and (idx > latest_digit_idx):
-                    if (latest_digit_idx := idx) == last_idx and earlist_digit_idx == 0:
-                        break          
-            calibration_value_digits += int(f"{line[earlist_digit_idx]}{line[latest_digit_idx]}")         
-        print(calibration_value_digits)
+                    if (latest_digit_idx := idx) == last_idx and earliest_digit_idx == 0:
+                        break  
+            
+            first_digit = line[earliest_digit_idx]
+            last_digit = line[latest_digit_idx]
+
+            calibration_value_only_digits += int(f"{first_digit}{last_digit}")
+
+            earliest_spelling_idx = earliest_digit_idx
+            latest_spelling_idx = latest_digit_idx
+
+            for spelling in spellings.keys():
+                lline = line[0:earliest_spelling_idx]
+                if len(spelling) <= len(lline):
+                    idx = lline.find(spelling, 0, earliest_spelling_idx)
+                    if idx != -1: 
+                        earliest_spelling_idx = idx + 1
+                        first_digit = spellings[spelling]
+                        if earliest_spelling_idx == 0 and latest_spelling_idx == last_idx:
+                            break
+            
+                rline = line[latest_spelling_idx + 1: last_idx + 1]
+                if len(spelling) <= len(rline):
+                    idx = rline.find(spelling, 0, last_idx + 1)
+                    if (idx != -1):
+                        latest_spelling_idx += idx + len(spelling)
+                        last_digit = spellings[spelling]
+                        if latest_spelling_idx == last_idx and earliest_spelling_idx == 0:
+                            break
+
+            
+            cal_val = int(f"{first_digit}{last_digit}")
+            print(f"'{line}':{cal_val}")
+            calibration_value_both += cal_val
+
+        print(f"Digits Only: {calibration_value_only_digits}")
+        print(f"Digits And Spellings: {calibration_value_both}")
