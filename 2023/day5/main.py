28f8e1b165729b4b07b2c841278fcaa4d023215d
StjerneIdioten <StjerneIdioten@gmail.com>
2023-12-12 18:26:55 +0100

    Something

--- 2023/day5/main.py
+++ 2023/day5/main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import time
+from itertools import batched
 
 with open(Path(__file__).parent / "test") as file:
     # Seeds
@@ -8,6 +8,8 @@ with open(Path(__file__).parent / "test") as file:
     file.readline()
 
     print(f"seeds: {seeds}")
+    seed_ranges = list(batched(seeds, 2))
+    print(f"Seed Ranges: {seed_ranges}")
 
     almanac = {}
     while True:
@@ -17,35 +19,42 @@ with open(Path(__file__).parent / "test") as file:
             almanac[label].append(tuple(map(int, line.rstrip('\n').split())))
 
         almanac[label].sort(key=lambda x: x[1])
-        print(f"{label}:")
-        for val in almanac[label]:
-            print(f"\t{val[0]:010} {val[1]:010} {val[2]:010}")
+        #print(f"{label}:")
+        #for val in almanac[label]:
+        #    print(f"\t{val[0]:010} {val[1]:010} {val[2]:010}")
         if not line:
             break
     
-    def search_almanac_mapping(mapping, index):
-        for value in mapping:
-            if index < value[1]:
-                break
-            elif index <= value[1] + value[2] - 1:
-                return index + value[0] - value[1]
-        return index
+    #def search_almanac_mapping(mapping, index):
+    #    for value in mapping:
+    #        if index < value[1]:
+    #            break
+    #        elif index <= value[1] + value[2] - 1:
+    #            return index + value[0] - value[1]
+    #    return index
 
-    start_time = time.time()
+    def search_almanac_mapping(mapping, indexes):
+        for idx in indexes:
+            for value in mapping:
+                if idx[0] < value[1]:
+                    pass
+                
 
-    seed_to_location = []
-    for index in seeds:
-        seed = index
-        print(f"Seed: {seed}")
-        for name, mapping in almanac.items():
-            index = search_almanac_mapping(mapping, index)
-            print(f"\t{name}: {index:03}")
-        seed_to_location.append((seed, index))
+        return indexes
     
-    print(f"Time: {(((time.time() - start_time) / len(seeds)) * 1176123349) / 1000}")
+    print(search_almanac_mapping(almanac['seed-to-soil'], seed_ranges))
 
-    for mapping in sorted(seed_to_location, key=lambda x: x[1], reverse=True):
-        print(f"Location: {mapping[1]}, Seed: {mapping[0]}")
+    #seed_to_location = []
+    #for index in seeds:
+    #    seed = index
+    #    print(f"Seed: {seed}")
+    #    for name, mapping in almanac.items():
+    #        index = search_almanac_mapping(mapping, index)
+    #        print(f"\t{name}: {index:03}")
+    #    seed_to_location.append((seed, index))
 
-    print(f"Lowest Location Number: {min(seed_to_location, key=lambda x: x[1])}")
+    #for mapping in sorted(seed_to_location, key=lambda x: x[1], reverse=True):
+    #    print(f"Location: {mapping[1]}, Seed: {mapping[0]}")
+
+    #print(f"Lowest Location Number: {min(seed_to_location, key=lambda x: x[1])}")
         
